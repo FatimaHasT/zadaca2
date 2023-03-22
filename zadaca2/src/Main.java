8f8f8780bec46d9fbee26afd0c78319b163400f0
Fatima Hasanovic <fatimahasanovic1994@gmail.com>
2023-03-22 12:27:56 +0100

    Zadaca 2

---
+++ zadaca2/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+
+    /*int r=2; //unjeti u cm
+    float PI= 3.14159265358979323846F;
+    float povrsina = r*r*PI;
+        System.out.println("Povrsina kruga je: " + povrsina + " cm2");*/
+
+       /* int numA=18;
+        int numB=3;
+
+        if (numA>10 && numA%3==0 && numA<10000 && numA%numB==0) {
+            System.out.println("The entered numbers meet all conditions");
+        }
+        else {
+            System.out.println("The entered numbers do not meet one or more conditions");*/
+
+        /*int numA=20;
+        int numB=10;
+
+
+        if (numA>numB){
+            System.out.println(numA+numB);
+
+        }
+
+        else if (numB>numA) {
+            System.out.println(numB-numA);
+        }
+
+        else if (numA==numB) {
+            System.out.println(numA*numB);
+        }*/
+
+        /*int x=78;
+        int y = ((x + x + 6) / 2) - x;
+        if (y%2==0){
+            System.out.println("The number y is: " + y + " and is divisible by 2");
+        }
+        else
+            System.out.println("The number y is: " + y + " and is not divisible by 2");
+         */
+
+        /*int numA = 5;
+        int numB = 8;
+        int numC = 4;
+        int numD = 6;
+
+        if ((numA > 0 && numB > 0) || (numC > 0 && numD > 0)) {
+            int sum = numA + numB + numC + numD;
+            System.out.println("The sum of all numbers is: " + sum);
+
+            if (sum > 100) {
+                if (sum % 2 == 0) {
+                    System.out.println("And that sum divided by 2 is: " + sum / 2);
+                } else {
+                    sum = sum - 1;
+                    if (sum % 2 == 0) {
+                        System.out.println("And that sum divided by 2 is: " + sum / 2);
+                    }
+                    else {
+                        System.out.println("The number is still not divisible by 2");
+                    }
+                }
+            } else {
+                System.out.println("The number is not greater then 100");
+            }
+        }*/
+
+
+
+        
+
+    }
+}
